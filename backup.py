9ef88c86eef274bcd991a52384a9bd6af34f9d2a
johnashu <johnashu1@hotmail.com>
2017-04-14 02:13:06 +0200

    Add files via upload

--- backup.py
+++ backup.py
@@ -3,12 +3,8 @@ import shutil, os, datetime, time, sys
 newFolderName = datetime.datetime.now()
 newFolder = newFolderName.strftime('%m%d%Y')
 
-
 src = ('E:\\COPY\\')
 dst = ('D:\\COPY\\' +
            newFolderName.strftime('%d-%B-%y\\').upper())
 
-for file in os.listdir(src):
-    src_file = os.path.join(src, file)
-    dst_file = os.path.join(dst, file)
-    shutil.copytree(src, dst)
+shutil.copytree(src, dst)
